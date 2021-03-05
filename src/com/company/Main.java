c1226ed9907e56d111a688860f2436d5da727822
vasyaleskiv <vasyaleskiv@gmail.com>
2021-03-05 20:08:47 +0200

    Learn variables

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "Roman lox";
+        int age = 22;
+        String colorHeir = "Syvyi";
+
+    }
+}
