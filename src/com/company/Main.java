010ae5dc65cf1f6dcaab1a396c09df6d43680650
vasyaleskiv <vasyaleskiv@gmail.com>
2021-03-05 21:32:31 +0200

    Average number

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,48 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int cocklength = 4;
-        if (cocklength < 5) {
-            System.out.println("Roman");
-        } else {
-            System.out.println("Vasya");
-        }
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Input the 1st number: ");
+        int num1 = input.nextInt();
+
+        System.out.print("Input the 2nd number: ");
+        int num2 = input.nextInt();
+
+        System.out.print("Input the 3rd number: ");
+        int num3 = input.nextInt();
+
+
+        if (num1 > num2)
+            if (num1 < num3)
+                System.out.println("The greatest: " + num1);
+
+        if (num2 < num1)
+            if (num2 > num3)
+                System.out.println("The greatest: " + num2);
+
+        if (num3 > num1)
+            if (num3 < num2)
+                System.out.println("The greatest: " + num3);
+
+
+        if (num1 < num2)
+            if (num1 > num3)
+                System.out.println("The greatest: " + num1);
+
+        if (num2 > num1)
+            if (num2 < num3)
+                System.out.println("The greatest: " + num2);
+
+        if (num3 < num1)
+            if (num3 > num2)
+                System.out.println("The greatest: " + num3);
+
+
+
+
     }
 }
